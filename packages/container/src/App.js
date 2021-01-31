42bb54b1c71cfb2a300f00d66ae14c15a9d7438f
ldco2016 <renaissance.scholar2012@gmail.com>
2021-01-31 07:44:10 -0600

    updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return (
     <div>
-      <h1>Howdy Yall!</h1>
+      <h1>Howdy!</h1>
       <hr />
       <MarketingApp />
     </div>
