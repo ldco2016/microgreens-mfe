640dea8e6c074fe47e9f453bf1ad97b6a5c0b149
ldco2016 <renaissance.scholar2012@gmail.com>
2021-02-01 17:41:00 -0600

    changed logo

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import ToolBar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
-import { Link as RouterLink } from 'react-router-dom';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import ToolBar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link as RouterLink } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  '@global': {
+  "@global": {
     ul: {
       margin: 0,
       padding: 0,
-      listStyle: 'none',
+      listStyle: "none",
     },
     a: {
-      textDecoration: 'none',
+      textDecoration: "none",
     },
   },
   appBar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
   },
   toolbar: {
-    flexWrap: 'wrap',
-    justifyContent: 'space-between'
+    flexWrap: "wrap",
+    justifyContent: "space-between",
   },
   link: {
-    margin: theme.spacing(1, 1.5)
+    margin: theme.spacing(1, 1.5),
   },
   heroContent: {
-    padding: theme.spacing(8, 0, 6)
-  }
+    padding: theme.spacing(8, 0, 6),
+  },
 }));
 
-export default function Header({signedIn, onSignOut}) {
+export default function Header({ signedIn, onSignOut }) {
   const classes = useStyles();
 
   const onClick = () => {
@@ -44,30 +44,30 @@ export default function Header({signedIn, onSignOut}) {
   return (
     <React.Fragment>
       <AppBar
-        position="static"
-        color="default"
+        position='static'
+        color='default'
         elevation={0}
         className={classes.appBar}
       >
         <ToolBar className={classes.toolbar}>
           <Typography
-            variant="h6"
-            color="inherit"
+            variant='h6'
+            color='inherit'
             noWrap
             component={RouterLink}
-            to="/"
+            to='/'
           >
-            Microurb
+            Microurb Farms
           </Typography>
           <Button
-            color="primary"
-            variant="outlined"
+            color='primary'
+            variant='outlined'
             className={classes.link}
             component={RouterLink}
-            to={signedIn ? '/' : '/auth/signin'}
+            to={signedIn ? "/" : "/auth/signin"}
             onClick={onClick}
           >
-            {signedIn ? 'Logout' : 'Login'}
+            {signedIn ? "Logout" : "Login"}
           </Button>
         </ToolBar>
       </AppBar>
