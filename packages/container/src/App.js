977b2d2bcf88e203166db18a706e1aa39769ab33
ldco2016 <renaissance.scholar2012@gmail.com>
2021-02-03 09:49:52 -0600

    Integrating Auth into the Container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
 import MarketingApp from "./components/MarketingApp";
+import AuthApp from './components/AuthApp';
 import Header from "./components/Header";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'co'
+  productionPrefix: "co",
 });
 
 export default () => {
@@ -14,9 +19,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Switch>
+            <Route path="/auth" component={AuthApp} />
+            <Route path="/" component={MarketingApp} />
+          </Switch>
         </div>
-      </StylesProvider> 
+      </StylesProvider>
     </BrowserRouter>
   );
 };
