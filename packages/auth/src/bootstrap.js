fe98b092dc7268613586b9f66f8b5042ab582e4a
ldco2016 <renaissance.scholar2012@gmail.com>
2021-02-03 10:03:06 -0600

    Adding Initial State to Memory History

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createMemoryHistory, createBrowserHistory} from 'history';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
 
 // Mount function to start up the app
-const mount = (el, {onNavigate, defaultHistory}) => {
-  const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
 
   if (onNavigate) {
     history.listen(onNavigate);
   }
-  
+
   ReactDOM.render(<App history={history} />, el);
 
   return {
-    onParentNavigate({pathname: nextPathname}) {
-      const {pathname} = history.location;
-      
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
       if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
@@ -26,11 +30,11 @@ const mount = (el, {onNavigate, defaultHistory}) => {
 
 // If we are in development and in isolation
 // call mount immediately
-if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_auth-dev-root');
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
 
   if (devRoot) {
-    mount(devRoot, {defaultHistory: createBrowserHistory()});
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
