7901cec4b4af3027d701f5b9bb74aa0d86a3252c
ldco2016 <renaissance.scholar2012@gmail.com>
2021-02-05 23:12:27 -0600

    modified dashboard sub app name

--- packages/dashboard-react/config/webpack.prod.js
+++ packages/dashboard-react/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/dashboard/latest/",
+    publicPath: "/dashboard-react/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
