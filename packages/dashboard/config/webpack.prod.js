eedc9ae009248f637159fff1e816ff5e430b94f7
ldco2016 <renaissance.scholar2012@gmail.com>
2021-02-05 23:20:57 -0600

    gave easier naming convention to dashboard sub app

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/dashboard-react/latest/",
+    publicPath: "/dashboard/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
